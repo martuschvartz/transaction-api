cdb830ef65a5531d474c7c806f3aa0dd18674715
martuschvartz <schvartzmartina@gmail.com>
2026-06-04 16:04:39 -0300

    fix: green test for create transaction

--- src/main/java/com/example/transactions_api/repository/InMemoryTransactionDao.java
+++ src/main/java/com/example/transactions_api/repository/InMemoryTransactionDao.java
@@ -2,12 +2,22 @@ package com.example.transactions_api.repository;
 
 import com.example.transactions_api.interfaces.TransactionDao;
 import com.example.transactions_api.models.Transaction;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class InMemoryTransactionDao implements TransactionDao {
+
+    final Map<Long, Transaction> store;
+
+    public InMemoryTransactionDao(){
+        this.store = new ConcurrentHashMap<>();
+    }
+
     @Override
-    public Transaction createTransaction(long validId, String type, double amount) {
-        return null;
+    public Transaction createTransaction(long id, String type, double amount) {
+        Transaction newTransaction = new Transaction(type, amount);
+        store.put(id, newTransaction);
+        return newTransaction;
     }
 }
